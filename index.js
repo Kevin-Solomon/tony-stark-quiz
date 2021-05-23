8af0f56de22437d6d25b758a64d1491ceddc4cf8
Kevin-Solomon <kevinsolomon404@gmail.com>
2021-05-24 00:44:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var readlineSync = require('readline-sync');
+var chalk = require('chalk');
+var userName = readlineSync.question("Hi May i know your Name: ");
+console.log(`Welcome ${userName} How well DO YOU KNOW Tony Stark!!`);
+
+var questionArray = [
+  {
+    question:"Who is "+chalk.red("Tony Stark's")+ " Best Friend?? ",
+    answer:"Captain Rhodes"
+  }
+  ,{
+    question:"What was his"+ chalk.blue("first suit called")+ "?? ",
+    answer:"Mark 1"
+  },
+  {
+    question:"Who is Tony's love interest?? ",
+    answer:"Pepper Potts"
+  },
+  {
+    question:"Which team did " +chalk.underline.bgRed("Tony")+ "join in 2012? ",
+    answer:"Avengers"
+  },
+  {
+    question:"Did Tony sign the Sokovia Accords?? ",
+    answer:"Yes"
+  }
+]
+var score=0;
+var highScore = [
+  {
+    name:"Pepper",
+    score:10
+  },
+  {
+    name:"Rhodes",
+    score:10
+  }
+]
+
+function askQuestion(question,answer)
+{
+  var userAnswer = readlineSync.question(question)
+  if(userAnswer === answer){
+    score++;
+  }else{
+    score--
+  }
+  console.log("Your score is: ",score)
+}
+
+for(var i=0;i<questionArray.length;i++){
+  var currentQuestion = questionArray[i];
+  askQuestion(currentQuestion.question,currentQuestion.answer);
+}
+console.log("Congratulations you scored: ",score)
+
+for(var i=0;i<highScore.length;i++){
+  var currentScore = highScore[i];
+  console.log("High Score: ",currentScore.score, " By ",currentScore.name  )
+}
